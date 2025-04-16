47775e480ea120ec04be35370d40092aef5b6a2d
Alexander Liu <alexander_l_22@hotmail.com>
2025-04-15 23:32:09 -0400

    Solved problem 199

---
+++ 199-[MEDIUM]-Binary-Tree-Right-Side-View.py
@@ -0,0 +1,39 @@
+from typing import Optional, List
+from collections import deque
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    """
+    Intuition:
+        We perform a traversal layer by layer. At each layer,
+        we append the rightmost element i.e. the last element
+        in the queue.
+    """
+
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+
+        q = deque([root])
+        view = []
+
+        while q:
+            # Append rightmost elmt
+            view.append(q[-1].val)
+
+            for i in range(len(q)):
+                node = q.popleft()
+
+                if node.left:
+                    q.append(node.left)
+
+                if node.right:
+                    q.append(node.right)
+
+        return view
