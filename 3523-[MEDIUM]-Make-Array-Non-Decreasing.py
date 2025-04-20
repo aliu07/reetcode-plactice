c0b1c1e3397277b7ebf6cfd358fa6364fe4eea32
Alexander Liu <alexander_l_22@hotmail.com>
2025-04-20 00:20:34 -0400

    Solved problem 3523

---
+++ 3523-[MEDIUM]-Make-Array-Non-Decreasing.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class Solution:
+    """
+    Intuition:
+        The problem is essentially just asking us to return a non-decreasing
+        array in disguise. To do this, we keep track of the largest value
+        encountered.
+    """
+
+    def maximumPossibleSize(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
+        res = []
+
+        for num in nums:
+            if not res:
+                res.append(num)
+            elif num >= res[-1]:
+                res.append(num)
+
+        return len(res)
