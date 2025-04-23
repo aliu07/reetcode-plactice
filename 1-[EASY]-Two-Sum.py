1a281d9f50df1cbb1c483d4f5cdd1549ed3292e6
Alexander Liu <alexander_l_22@hotmail.com>
2025-04-22 22:35:36 -0400

    Solved problem 1

---
+++ 1-[EASY]-Two-Sum.py
@@ -0,0 +1,27 @@
+from typing import List
+
+class Solution:
+    """
+    Intuition:
+        Rather than brute forcing, we can store number in a hashmap and
+        simply perform a lookup for subsequent numbers to see if the
+        difference between the target number and the current number is
+        contained within the hashmap
+
+    Notes:
+        We need to return the indices in order. Since we iterate from left
+        to right, we know that if target - num is in the map, then the index
+        associated will be smaller by default.
+    """
+
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        map = {}
+
+        for i, num in enumerate(nums):
+            if target - num in map:
+                return [map[target - num], i]
+
+            map[num] = i
+
+        # Should never get here
+        return [-1, -1]
