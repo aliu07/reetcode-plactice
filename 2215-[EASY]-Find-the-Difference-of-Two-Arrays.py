7c692e7ddd1e0d1e9c47af12c03ce5162ed60373
Alexander Liu <alexander_l_22@hotmail.com>
2025-04-22 23:51:16 -0400

    Solved problem 2215

---
+++ 2215-[EASY]-Find-the-Difference-of-Two-Arrays.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    """
+    Intuition:
+        Accelerate lookups by hasing each array into a hashset.
+
+    Runtime: O(n)
+
+    Memory: O(n)
+    """
+
+    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        set1, set2 = set(nums1), set(nums2)
+        ans = [
+            [elmt for elmt in set1 if elmt not in set2],
+            [elmt for elmt in set2 if elmt not in set1]
+        ]
+
+        return ans
