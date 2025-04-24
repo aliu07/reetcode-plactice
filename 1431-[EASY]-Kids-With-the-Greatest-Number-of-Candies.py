4a6e14934d4fdb24d5516f261e1193e7b5632404
Alexander Liu <alexander_l_22@hotmail.com>
2025-04-24 19:06:27 -0400

    Solved problem 1431

---
+++ 1431-[EASY]-Kids-With-the-Greatest-Number-of-Candies.py
@@ -0,0 +1,27 @@
+from typing import List
+
+class Solution:
+    """
+    Intuition:
+        Find kid with most candies with first pass. Then, for each kid, we compare
+        the candies they have + the amount of extra candies and see if it exceeds the
+        max previously found.
+
+    Runtime: O(n)
+
+    Memory: O(n), but O(1) auxiliary
+    """
+
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        maxCandies = 0
+
+        for amt in candies:
+            maxCandies = max(maxCandies, amt)
+
+        res = [False] * len(candies)
+
+        for i in range(len(res)):
+            if candies[i] + extraCandies >= maxCandies:
+                res[i] = True
+
+        return res
