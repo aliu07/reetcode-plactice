fe121264fff8daab1f8894ab0241e82b34be3c06
Alexander Liu <alexander_l_22@hotmail.com>
2025-06-05 17:48:47 -0400

    Solved problem 1657

---
+++ 1657-[MEDIUM]-Determine-if-Two-Strings-Are-Close.py
@@ -0,0 +1,43 @@
+class Solution:
+    """
+    Intuition:
+        We are given 2 possible operations:
+            1) Swap any two existing characters in a word
+            2) Transform every occurrence of one existing character into another existing
+               character, and do the same with the other character.
+
+        What does this mean for us? It means that 2 strings are close as long as every
+        character in word1 also exists in word2 and the frequencies (occurrences of each
+        character) match up.
+
+    Runtime:
+        O(n) since we need to build the hashsets and frequence counting arrays.
+
+    Memory:
+        O(n) to store the hashsets and arrays.
+
+    Notes:
+        The runtime of this solution is very slow on the LeetCode platform. We can rely on
+        built-in data structures to accelerate our performance significantly.
+    """
+
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        if len(word1) != len(word2):
+            return False
+
+        # Check available chars
+        chars1 = set()
+        chars2 = set()
+
+        # Check frequency of chars
+        freq1 = [0] * 26
+        freq2 = [0] * 26
+
+        for c1, c2 in zip(word1, word2):
+            chars1.add(c1)
+            chars2.add(c2)
+
+            freq1[ord(c1) - ord('a')] += 1
+            freq2[ord(c2) - ord('a')] += 1
+
+        return sorted(freq1) == sorted(freq2) and chars1 == chars2
