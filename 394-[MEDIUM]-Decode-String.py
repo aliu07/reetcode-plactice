8bc1f5384c729cce31e857661c2d8ae07678ec67
Alexander Liu <alexander_l_22@hotmail.com>
2025-06-06 21:29:29 -0400

    Add updated solution to problem 394

--- 394-[MEDIUM]-Decode-String.py
+++ 394-[MEDIUM]-Decode-String.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution1:
     """
     Intuition:
         The main pattern we need to observe is that we only ever need to decode
@@ -51,3 +51,65 @@ class Solution:
             ix += 1
 
         return ''.join(stack)
+
+
+
+class Solution2:
+    """
+    Intuition:
+        Instead of a singular stack, we can opt to use 2 distinct stacks. One holds
+        all the multipliers while the other holds all the char sequences.
+
+        Case 1) If the current character is a digit (0-9), append it to currMul.
+
+        Case 2) If the current character is a letter (a-z), append it to currSeq.
+
+        Case 3) If current character is a opening bracket [, push currMul and currSeq
+                onto their respective stacks.
+
+        Case 4) Closing bracket ]: We must begin the decoding process,
+
+                We must decode the currSeq. Pop the multiplier from its stack and decode
+                the pattern multiplier[currSeq]
+
+                As the sequences stack contains a possible previously decoded string, pop
+                that sequence from the sequences stack.
+                Update the decodedString = decodedString + multiplier[currSeq]
+
+    Runtime:
+        O(maxK * n) since each substring is built once and then repeated 'k' times
+        unliked the single-stack approach which may re-decode the same substring
+        multiple times due to nesting.
+
+    Memory:
+        O(m + n) where n is the number of a-z chars in the input string and m is
+        the number of digit chars in the input string.
+    """
+
+    def decodeString(self, s: str) -> str:
+        sequences = []
+        multipliers = []
+
+        currSeq = ""
+        currMul = ""
+
+        for char in s:
+            if char.isdigit():
+                currMul += char
+            elif char == '[':
+                multipliers.append(int(currMul))
+                sequences.append(currSeq)
+
+                # Reset values
+                currSeq, currMul = "", ""
+            elif char == ']':
+                decoded = sequences.pop()
+                multiplier = multipliers.pop()
+
+                decoded = decoded + currSeq * multiplier
+
+                currSeq = decoded
+            else:
+                currSeq += char
+
+        return currSeq
