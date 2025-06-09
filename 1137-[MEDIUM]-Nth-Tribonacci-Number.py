bcb5a7fb7a455339e05aae887c375fa9be2817f6
Alexander Liu <alexander_l_22@hotmail.com>
2025-06-09 12:16:43 -0400

    Add constant memory solution to problem 1137

--- 1137-[MEDIUM]-Nth-Tribonacci-Number.py
+++ 1137-[MEDIUM]-Nth-Tribonacci-Number.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution1:
     """
     Intuition:
         Build a bottom up dp table to perform lookups. We use previously
@@ -23,3 +23,34 @@ class Solution:
             dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1]
 
         return dp[-1]
+
+
+
+class Solution2:
+    """
+    Intuition:
+        We can remove the dp array by noticing that we only ever need the 3
+        previous numbers in the sequence. Therefore, we can store them in
+        separate variables and update them upon each iteration.
+
+    Runtime:
+        Still O(n)
+
+    Memory:
+        O(1) now since we removed the need for a dp cache.
+    """
+    def tribonacci(self, n: int) -> int:
+        # Base cases
+        t0, t1, t2 = 0, 1, 1
+
+        if n == 0:
+            return t0
+        elif n == 1:
+            return t1
+        elif n == 2:
+            return t2
+
+        for i in range(3, n + 1):
+            t0, t1, t2 = t1, t2, t0 + t1 + t2
+
+        return t2
