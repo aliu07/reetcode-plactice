baf0098dc840ffebd8d840e2630fe2bb41c388d3
Alexander Liu <alexander_l_22@hotmail.com>
2025-06-24 17:07:14 -0400

    Add optimized constant space solution

--- 2130-[MEDIUM]-Maximum-Twin-Sum-of-a-Linked-List.py
+++ 2130-[MEDIUM]-Maximum-Twin-Sum-of-a-Linked-List.py
@@ -37,3 +37,65 @@ class Solution1:
             sums.append(nums[i] + nums[-1 - i])
 
         return max(sums)
+
+
+
+class Solution2:
+    """
+    Intuition:
+        We are guaranteed that the linked list has an even number of
+        elements. By reversing the 2nd half of the linked list, we can
+        obtain 2 sublists that we can then traverse simultaneously to
+        compute twin sums. This saves us from allocating extra space
+        for an array.
+
+    Runtime:
+        O(n) to traverse the linked list and find its length.
+
+    Memory:
+        O(1) since we don't need to allocate any other data structure.
+    """
+
+    def pairSum(self, head: Optional[ListNode]) -> int:
+        if not head:
+            return 0
+
+        # Figure out the length of the list
+        N = 0
+        curr = head
+
+        while curr:
+            N += 1
+            curr = curr.next
+
+        # Reverse 2nd half
+        curr = head
+
+        for i in range(N // 2 - 1):
+            curr = curr.next
+
+        # Vars to reverse 2nd half of linked list
+        rev_head, prev = curr.next, None
+        # Break link between 1st and 2nd half
+        curr.next = None
+
+        while rev_head:
+            tmp = rev_head
+            rev_head = rev_head.next
+            tmp.next = prev
+            prev = tmp
+
+        # Traverse 1st half and reverse 2nd half to find max twin sum
+        res = 0
+        l1, l2 = head, prev
+
+        while l1:
+            sum = l1.val + l2.val
+
+            if sum > res:
+                res = sum
+
+            l1 = l1.next
+            l2 = l2.next
+
+        return res
