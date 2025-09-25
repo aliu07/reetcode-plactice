7407e340368959b11a76a84ca0d037d7a4ab3b1e
Alexander Liu <alexander_l_22@hotmail.com>
2025-09-25 16:45:49 -0400

    [104][easy] add recursive soln

--- 0104-[EASY]-Maximum-Depth-of-Binary-Tree.py
+++ 0104-[EASY]-Maximum-Depth-of-Binary-Tree.py
@@ -10,16 +10,42 @@ class TreeNode:
         self.right = right
 
 
-class Solution:
+class Solution1:
+    """
+    Intuition:
+        Use a recursive approach with a base case and a recursive step.
+
+    Runtime:
+        O(n) since we need to process each node once.
+
+    Memory:
+        O(1) auxiliary space.
+
+        Call stack is O(n) in the worst case for an unbalanced tree.
+
+        So overall constant auxiliary memory, but noteworthy to look
+        at how call stack scales.
+    """
+
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+
+class Solution2:
     """
     Intuition:
         This problem is fairly straightforward. He simply use either BFS or DFS to
         process each node in the binary tree and determine if its depth is the
         greatest.
 
-    Runtime: O(n) where n is the number of nodes in the binary tree
+    Runtime:
+        O(n) where n is the number of nodes in the binary tree
 
-    Memory: O(n) where n is the number of nodes in the binary tree
+    Memory:
+        O(n) where n is the number of nodes in the binary tree
     """
 
     def maxDepth(self, root: Optional[TreeNode]) -> int:
