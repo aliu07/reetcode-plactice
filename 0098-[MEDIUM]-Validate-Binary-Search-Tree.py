7aad1036a5fcbdde61bd09d0523f6c9c8cbcbeef
Alexander Liu <alexander_l_22@hotmail.com>
2025-10-03 21:29:03 -0400

    [98][medium] solved validate bst problem

---
+++ 0098-[MEDIUM]-Validate-Binary-Search-Tree.py
@@ -0,0 +1,58 @@
+from typing import Optional
+from math import inf
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    """
+    Intuition:
+        We can reason each node as having a left and right bound. The
+        node's value has to fall within that specific bound.
+
+        Initial condition: at the root, there are no bound constraints
+        yet (we will build them as we go), so the left and right bounds
+        are respectively -inf and inf.
+
+        As we traverse the tree, we can enforce new constraints on the
+        child nodes.
+
+        The child composing the left subtree will need to enforce an
+        upper bound equal to the current node's value sin it and all
+        of its children must have values less than the current node.
+
+        The child composing the right subtree will need to enforce a
+        lower bound equal to the current node's value since it and all
+        of its children must have values greater than the current node.
+
+        This solution takes a recursive approach in solving the problem
+        with the outlined ideas.
+
+    Runtime:
+        Each node has to be processed once to validate the BST, so O(n)
+        runtime.
+
+    Memory:
+        The call stack of the helper dfs function has size O(h) where h
+        is the height of the tree. Thus, we have O(log n) in the best case
+        and O(n) in the worst case with an unbalanced tree.
+    """
+
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def dfs(node: TreeNode, l: int, r: int):
+            if not node:
+                return True
+
+            # violates BST property
+            if node.val <= l or node.val >= r:
+                return False
+
+            return dfs(node.left, l, node.val) and dfs(node.right, node.val, r)
+
+        return dfs(root, -inf, inf)
