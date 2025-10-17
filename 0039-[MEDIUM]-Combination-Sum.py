c5761bc6ca4a91c30ce93efcd0d3a359f98b1345
Alexander Liu <alexander_l_22@hotmail.com>
2025-10-17 17:11:10 -0400

    [39][medium] add optimized backtracking soln

--- 0039-[MEDIUM]-Combination-Sum.py
+++ 0039-[MEDIUM]-Combination-Sum.py
@@ -1,7 +1,7 @@
 from typing import List
 
 
-class Solution:
+class Solution1:
     """
     Intuition:
         We use a brute-force DFS approach to solve this problem. At each step, we
@@ -46,3 +46,59 @@ class Solution:
 
         dfs([], 0)
         return [list(elmt) for elmt in res]
+
+
+class Solution2:
+    """
+    Intuition:
+        We can introduce 3 optimizations.
+
+        The first optimization is to sort the nums array. This allows us to
+        terminate early if the number at the current index exceeds the target.
+
+        The second optimization is in managing the construction of our combinations.
+        Instead of having to sort each combination to check uniqueness, we use
+        a starting index to indicate which number we start building our current
+        combinations from. This way, we ensure that each combination is sorted
+        by default.
+
+        Lastly, instead of trying every single element in nums, we reduce our
+        choices to 2 options:
+        - Add the number at the current start index to our current combination.
+        - Skip it and move onto the next number by incrementing the start index.
+
+    Runtime:
+        Our longest combination can be of length target (combination of all 1's).
+
+        At each recursive step, we are presented with 2 options. Given that the
+        longest combination is of length target, the time complexity is O(2^t).
+
+        This is better than O(n^t) from Solution1 since we reduced n choices to
+        2.
+
+    Memory:
+        The call stack takes O(h) where h is the height of the recursion tree.
+        In the worst case, we can have a combination of length target (if nums
+        contains 1). Therefore, the overall memory complexity is O(target).
+    """
+
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        res = []
+        nums.sort()
+
+        def dfs(startIx, currNums, currSum):
+            if currSum == target:
+                res.append(currNums)
+                return
+            elif currSum > target or startIx >= len(nums):
+                return
+            elif nums[startIx] > target:
+                return
+
+            # add num at startIx
+            dfs(startIx, currNums + [nums[startIx]], currSum + nums[startIx])
+            # move onto next num
+            dfs(startIx + 1, currNums, currSum)
+
+        dfs(0, [], 0)
+        return res
