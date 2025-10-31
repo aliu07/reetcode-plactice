19ebb5404a9f79b63340e2750711425d7bb1ad05
Alexander Liu <alexander_l_22@hotmail.com>
2025-10-30 23:03:44 -0400

    [200][medium] add recursive dfs soln

--- 0200-[MEDIUM]-Number-of-Islands.py
+++ 0200-[MEDIUM]-Number-of-Islands.py
@@ -2,7 +2,7 @@ from collections import deque
 from typing import List
 
 
-class Solution:
+class Solution1:
     """
     Intuition:
         We iterate through the grid. When we hit an island, we
@@ -51,5 +51,44 @@ class Solution:
                             q.append((row - 1, col))
                             q.append((row, col + 1))
                             q.append((row, col - 1))
+
+        return islands
+
+
+class Solution2:
+    """
+    Intuition:
+        Same as Solution1. Instead, we use a recursive DFS
+        approach.
+
+    Runtime:
+        Same as Solution1.
+
+    Memory:
+        Same as Solution1.
+    """
+
+    def numIslands(self, grid: List[List[str]]) -> int:
+        islands = 0
+        M, N = len(grid), len(grid[0])
+
+        def dfs(r, c):
+            if r < 0 or r >= M or c < 0 or c >= N:
+                return
+
+            if grid[r][c] != "1":
+                return
+
+            grid[r][c] = "."
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+
+        for r in range(M):
+            for c in range(N):
+                if grid[r][c] == "1":
+                    islands += 1
+                    dfs(r, c)
 
         return islands
