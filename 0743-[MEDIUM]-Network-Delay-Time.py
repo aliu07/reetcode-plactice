43229b09f99903f1c7d462e84ec3918c52f6bd52
Alexander Liu <alexander_l_22@hotmail.com>
2025-11-29 20:18:06 -0500

    [743][medium] solved network delay

---
+++ 0743-[MEDIUM]-Network-Delay-Time.py
@@ -0,0 +1,69 @@
+from collections import deque
+from math import inf
+from typing import List
+
+
+class Solution:
+    """
+    Intuition:
+        We start by building an adjancency list of our edges. Then, we
+        use DFS to traverse the graph and compute the minimum times to
+        reach every other node from k, maintaining a look-up table to do
+        so. Finally, we take the largest time in the table and return it
+        as our result.
+
+    Runtime:
+        Let E be the number of edges and V be the number of nodes.
+
+        Building the adjacency list takes O(E) time.
+
+        Performing DFS does not take the usual O(V + E) time. The current
+        implementation does not guarantee that each node's distance improves
+        only once. DFS can discover "heavier paths" first and then find
+        shorter ones. When this happens, we incur the cost of re-exploring
+        all outgoing edges of the node which takes at most O(E) time. Each
+        node's distance can improve at most V times (we come from a
+        different node each time).
+
+        Finding the largest delay in the times array takes O(V) time.
+
+        Overall, O(V * E) time.
+
+    Memory:
+        The adjacency list takes O(E) space.
+
+        The DFS queue takes at most O(E) space (all outgoing edges
+        present).
+
+        The times array takes O(V) space.
+
+        Thus, overall memory complexity of O(V + E).
+    """
+
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        # build adj list with weights/delays
+        adj = [[] for _ in range(n + 1)]
+        for u, v, delay in times:
+            adj[u].append((v, delay))
+
+        # init DFS
+        q = deque([(k, 0)])
+        # keep track of min time to reach each node from k
+        times = [inf for _ in range(n + 1)]
+        times[0] = 0  # index 0 is tombstone since nodes are 1-indexed
+
+        # perform DFS
+        while q:
+            node, time = q.pop()
+
+            if time < times[node]:
+                times[node] = time
+
+                # only keep exploring if it's worth it
+                # i.e. current time was a minimum
+                for nei, delay in adj[node]:
+                    q.append((nei, time + delay))
+
+        # compute result
+        res = max(times)
+        return res if res != inf else -1
