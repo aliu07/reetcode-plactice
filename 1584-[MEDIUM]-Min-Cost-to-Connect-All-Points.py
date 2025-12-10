9266d3081d1297850a8ce479f8477f3cd79b488e
Alexander Liu <alexander_l_22@hotmail.com>
2025-12-10 15:41:05 -0500

    [1584][medium] add prims algo soln

--- 1584-[MEDIUM]-Min-Cost-to-Connect-All-Points.py
+++ 1584-[MEDIUM]-Min-Cost-to-Connect-All-Points.py
@@ -1,7 +1,9 @@
+import heapq
+from collections import defaultdict
 from typing import List
 
 
-class Solution:
+class Solution1:
     """
     Intuition:
         We can use a greedy approach here to always take the edge that
@@ -79,5 +81,65 @@ class Solution:
         for dist, u, v in pairs:
             if union(u, v):
                 res += dist
+
+        return res
+
+
+class Solution2:
+    """
+    Intuition:
+        Use Prim's Algorithm. It relies on keeping track of connected
+        points through a hash set and greedily taking the min-cost edge
+        via a min-heap.
+
+    Runtime:
+        Building the adjacency list takes O(n^2) time.
+
+        Each pair of points is processed at most once. The min-heap
+        contains at most all pairs. Thus, pushing and popping from
+        this min-heap takes at most O(log n^2) ~ O(log n). Since we
+        have up to O(n^2) unique pairs, the total runtime to explore
+        all points is O(n^2 * log n).
+
+        Thus, total runtime is O(n^2 * log n).
+
+    Memory:
+        Adjacency list takes O(n^2) space.
+
+        Visited set takes O(n) space.
+
+        Min-heap takes at most O(n^2) space.
+
+        Thus, O(n^2) overall space complexity.
+    """
+
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        N = len(points)
+
+        # build adj list
+        adj = defaultdict(list)
+        for i in range(N):
+            x1, y1 = points[i]
+
+            for j in range(i + 1, N):
+                x2, y2 = points[j]
+                dist = abs(x1 - x2) + abs(y1 - y2)
+                adj[i].append((dist, j))
+                adj[j].append((dist, i))
+
+        res = 0
+        visited = set()
+        min_heap = [(0, 0)]
+        while len(visited) < N:
+            cost, i = heapq.heappop(min_heap)
+
+            if i in visited:
+                continue
+
+            res += cost
+            visited.add(i)
+            for nei_cost, nei in adj[i]:
+                if nei not in visited:
+                    heapq.heappush(min_heap, (nei_cost, nei))
 
         return res
