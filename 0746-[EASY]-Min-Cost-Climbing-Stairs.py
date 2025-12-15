059b9b1448c8ba7af76d9de8a1105b9db9d40dc4
Alexander Liu <alexander_l_22@hotmail.com>
2025-12-14 23:30:13 -0500

    [746][easy] solved min cost climbing stairs

---
+++ 0746-[EASY]-Min-Cost-Climbing-Stairs.py
@@ -0,0 +1,32 @@
+import math
+from typing import List
+
+
+class Solution:
+    """
+    Intuition:
+        Define base cases, then build dp array from bottom
+        up.
+
+        At the end, we can either take 1 step of 2 steps to
+        reach top, so take min of last 2 elements.
+
+    Runtime:
+        O(n)
+
+    Memory:
+        O(n)
+    """
+
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        N = len(cost)
+        dp = [math.inf] * N
+        dp[0] = cost[0]
+        dp[1] = cost[1]
+
+        for i in range(2, N):
+            c1 = dp[i - 1] + cost[i]
+            c2 = dp[i - 2] + cost[i]
+            dp[i] = min(c1, c2)
+
+        return min(dp[-1], dp[-2])
