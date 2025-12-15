0c518af8ba739cab6765bfbe718c11107a2803e4
Alexander Liu <alexander_l_22@hotmail.com>
2025-12-15 13:19:56 -0500

    [198][medium] add space optimized soln

--- 0198-[MEDIUM]-House-Robber.py
+++ 0198-[MEDIUM]-House-Robber.py
@@ -1,7 +1,7 @@
 from typing import List
 
 
-class Solution:
+class Solution1:
     """
     Intuition:
         Decision to rob current house overlaps with sub-problem
@@ -34,3 +34,29 @@ class Solution:
             dp[i] = max(dp[i - 1], nums[i] + dp[i - 2])
 
         return dp[-1]
+
+
+class Solution2:
+    """
+    Intuition:
+        Same as Solution1.
+
+    Runtime:
+        O(n).
+
+    Memory:
+        O(1).
+    """
+
+    def rob(self, nums: List[int]) -> int:
+        N = len(nums)
+
+        if N == 1:
+            return nums[0]
+
+        rob1, rob2 = nums[0], max(nums[0], nums[1])
+
+        for i in range(2, N):
+            rob1, rob2 = rob2, max(rob2, rob1 + nums[i])
+
+        return rob2
