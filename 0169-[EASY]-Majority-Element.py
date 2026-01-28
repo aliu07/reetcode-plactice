a38271d43b8fde2dae65734a414696144faff068
Alexander Liu <alexander_l_22@hotmail.com>
2026-01-27 23:25:28 -0500

    [169][easy] add constant memory soln

--- 0169-[EASY]-Majority-Element.py
+++ 0169-[EASY]-Majority-Element.py
@@ -1,7 +1,7 @@
 from typing import List
 
 
-class Solution:
+class Solution1:
     """
     Intuition:
         Keep track of frequency, return the number who appears
@@ -25,3 +25,33 @@ class Solution:
 
         # should never get here
         return -1
+
+
+class Solution2:
+    """
+    Intuition:
+        Instead of counting frequency, we can manage state via a counter.
+        Since the majority element is guaranteed to appear more than n / 2
+        (i.e. half) the time, we can use the candidate variable to track
+        what we think will be the majority element. If the counter stays
+        positive, the candidate remains. If it ever becomes 0, we need
+        to change our candidate.
+
+    Runtime:
+        Still O(n).
+
+    Memory:
+        O(1).
+    """
+
+    def majorityElement(self, nums: List[int]) -> int:
+        count = 0
+        candidate = 0
+
+        for n in nums:
+            if count == 0:
+                candidate = n
+
+            count += 1 if n == candidate else -1
+
+        return candidate
