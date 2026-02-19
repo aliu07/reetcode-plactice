751318cd40956b48d6f4a07c24148f9289653e0f
Alexander Liu <alexander.liu22@outlook.com>
2026-02-18 21:10:18 -0500

    [696][easy] add optimized linear soln

--- 0696-[EASY]-Count-Binary-Substrings.py
+++ 0696-[EASY]-Count-Binary-Substrings.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution1:
     """
     Intuition:
         Brute force approach. We expand outward every time we hit a
@@ -32,5 +32,39 @@ class Solution:
                     res += 1
                     l -= 1
                     r += 1
+
+        return res
+
+
+class Solution2:
+    """
+    Intuition:
+        We can maintain the length of the previous consecutive
+        sequence in a `compressed` array. Combined with a 2 ptr
+        approach, we can ditch the inner loop.
+
+    Runtime:
+        O(N) as we reduced to a single linear pass.
+
+    Memory:
+        O(N) now since we need to maintain an array. In the worst
+        case, we have an alternating binary string like "0101010".
+    """
+
+    def countBinarySubstrings(self, s: str) -> int:
+        res = 0
+        compressed = [0]
+        l, r = 0, 0
+
+        while r < len(s):
+            if s[l] == s[r]:
+                # as long as our curr seq <= prev seq, we incr result
+                if r - l + 1 <= compressed[-1]:
+                    res += 1
+                r += 1
+            else:  # consecutive chain broken
+                #  save len of prev consecutive seq
+                compressed.append(r - l)  # no +1 since we exclude `r` ptr
+                l = r
 
         return res
